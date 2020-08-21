3faacf7f05e380a274bd1bc7264dfb2777aec789
AntowkaGor <antowka_gor@ukr.net>
2020-08-21 23:30:31 +0300

    toDO list

---
+++ VUEjs/hw_lesson1/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1>MY Todo list</h1>
+    <input type="text" v-model="text" placeholder="mast do IT" >
+    <button @click="add">➕</button> 
+    <ul>
+      <li  class="style_li" v-for="(item, i) in arr" 
+          :key="i">
+        <span>{{item}}</span>
+        <button @click="del(i)">🗑</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      text: '',
+      arr: []
+    }
+  },
+
+  methods: {
+    add() {
+      this.arr.push(this.text);
+      this.text = '';
+    },
+
+    del(i) {
+      this.arr.splice(i, 1)
+    }
+  }
+}
+</script>
+
+<style>
+.style_li{
+  display: flex;
+  justify-content: space-between;
+  width: 150px;
+  margin: 10px
+  }
+</style>
